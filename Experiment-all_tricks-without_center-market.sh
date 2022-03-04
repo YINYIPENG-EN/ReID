4d5c2dc79b2befb8a76eb7369c5063bfad3281a3
以鹏 <15930920977@163.com>
2022-03-04 17:57:46 +0800

    fist commit

---
+++ Experiment-all_tricks-without_center-market.sh
@@ -0,0 +1,11 @@
+# Experiment all tricks without center loss : 256x128-bs16x4-warmup10-erase0_5-labelsmooth_on-laststride1-bnneck_on
+# Dataset 1: market1501
+# imagesize: 256x128
+# batchsize: 16x4
+# warmup_step 10
+# random erase prob 0.5
+# labelsmooth: on
+# last stride 1
+# bnneck on
+# without center loss
+python3 tools/train.py --config_file='configs/softmax_triplet.yml' MODEL.DEVICE_ID "('0')" DATASETS.NAMES "('market1501')" DATASETS.ROOT_DIR "(r'./data')" OUTPUT_DIR "('E:/ReID/logs')"
